c8e58875f7c595b90529ee4c9d61cb87a547cd63
eliocostac4@gmail.com <eliocostac4@gmail.com>
2017-11-18 19:37:10 -0200

    teste basico finalizado

--- src/main/java/br/edu/utfpr/apsseleniumtest/TestClass.java
+++ src/main/java/br/edu/utfpr/apsseleniumtest/TestClass.java
@@ -1,28 +1,73 @@
 package br.edu.utfpr.apsseleniumtest;
 
+import java.util.List;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestClass {
     
-    public static void main(String[] args) throws InterruptedException {
-
+    public static boolean TestFormulario() throws InterruptedException{
         System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
-
-        // Create a new instance of the Firefox driver
+        
         WebDriver driver = new FirefoxDriver();
 
         driver.get("http://localhost:8080/");
-
-
-
-        // Print a Log In message to the screen
-        System.out.println("Opened");
-
-        //Wait for 5 Sec
-        Thread.sleep(5000);
-
-        // Close the driver
+        
+        driver.findElement(By.name("nome")).sendKeys("Elio");
+        driver.findElement(By.name("endereco")).sendKeys("Avenida Brasil");
+        driver.findElement(By.name("telefone")).sendKeys("46999392238");
+        driver.findElement(By.name("submit")).submit();
+        
+        WebElement myDynamicElement = (new WebDriverWait(driver, 10))
+                  .until(ExpectedConditions.presenceOfElementLocated(By.id("box-result")));
+        
+        WebElement element = driver.findElement(By.id("box-result"));
+        
+        System.out.println("----------");
+        System.out.println("Retorno do teste: " + element.getText());
+        System.out.println("----------");
+        
+        Thread.sleep(2000);
         driver.quit();
+        return true;
+    }
+    
+    public static boolean TesteGoogleSeach() throws InterruptedException{
+        
+        System.setProperty("webdriver.gecko.driver", "C:\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        
+        driver.get("http://www.google.com");
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("Cheese!\n"); // send also a "\n"
+        element.submit();
+
+        // wait until the google page shows the result
+        WebElement myDynamicElement = (new WebDriverWait(driver, 10))
+                  .until(ExpectedConditions.presenceOfElementLocated(By.id("resultStats")));
+
+        List<WebElement> findElements = driver.findElements(By.xpath("//*[@id='rso']//h3/a"));
+
+        // this are all the links you like to visit
+        for (WebElement webElement : findElements)
+        {
+            System.out.println(webElement.getAttribute("href"));
+        }
+        
+//        Wait for 5 Sec
+        Thread.sleep(2000);
+        
+        driver.quit();
+        return true;
+    }
+    
+    public static void main(String[] args) throws InterruptedException {
+        
+        TestFormulario();
+        TesteGoogleSeach();
     }
 }
